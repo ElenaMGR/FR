a29fc0b2ba7ceb10ef7411616cee718267c63cba
ElenaMGR <elenamgori@gmail.com>
2016-11-28 16:23:44 +0100

    Inclidos comentarios en la clase Usuario

--- FR_Ejercicio5/src/fr_ejercicio5/Usuario.java
+++ FR_Ejercicio5/src/fr_ejercicio5/Usuario.java
@@ -6,7 +6,7 @@
 package fr_ejercicio5;
 
 /**
- *
+ * Clase Usuario
  * @author Elena María Gómez Ríos y Jose Luis Martínez Ortíz
  */
 public class Usuario {
@@ -14,23 +14,44 @@ public class Usuario {
     String user;
     String pass;
 
+    /**
+     * Constructor parametrizado
+     * @param user Nombre del usuario
+     * @param pass Contraseña del usuario
+     */
     public Usuario(String user, String pass) {
         this.user = user;
         this.pass = pass;
     }
 
+    /**
+     * Método que devuelve el nombre del usuario
+     * @return nombre del usuario
+     */
     public String getUser() {
         return user;
     }
 
+    /**
+     * Método que asigna el nombre del usuario
+     * @param user nombre del usuario
+     */
     public void setUser(String user) {
         this.user = user;
     }
 
+    /**
+     * Método que devuelve la contraseña del usuario
+     * @return contraseña del usuario
+     */
     public String getPass() {
         return pass;
     }
 
+    /**
+     * Método que asigna la contraseña del usuario
+     * @param pass contraseña del usuario
+     */
     public void setPass(String pass) {
         this.pass = pass;
     }
